a3c89348c0c617feeba5ef8f3c4d8cd20d0b32c1
realmotty <89932696+realmotty@users.noreply.github.com>
2021-09-12 22:33:19 -0500

    Add files via upload

---
+++ Lab3Guess.java
@@ -0,0 +1,58 @@
+/* 
+Maurzel Castleberry
+Due Date: 09/12 | CS232
+
+
+*/
+package lab3guess;
+
+import java.util.Scanner;
+
+
+
+public class Lab3Guess {
+
+
+    public static void main(String[] args) {
+        
+    Scanner Input = new Scanner(System.in);
+        
+double rand = Math.random(); 
+        
+int num, tries=0, guess;
+int max = 100;
+int min = 1;
+int range = max - min + 1;
+  
+//GENERATES RANDOM NUMBER BETWEEN 1 AND 100
+for (int i = 0; i < 100; i++) {
+     num = (int)(Math.random() * range) + min;
+        
+        System.out.println("I'm thinking of a number between 1 and 100 ...\n");
+        System.out.println("Please guess a number; ");
+        guess = Input.nextInt();
+        
+
+    while (true) {
+
+	if (guess == num) 
+        {
+            System.out.println("Congratulations! You guessed the number in "+tries+" guess(es)!");
+            System.exit(1);
+    	} 
+	else if (guess > num)
+	{ 
+            System.out.println(" "+guess+" is too high.\n");
+            guess = Input.nextInt();
+	} 
+        else if (guess < num) 
+        { 
+            System.out.println(" "+guess+" is too low.\n");
+            guess = Input.nextInt();
+	}	 
+	tries+=1; 
+        }
+ //-------------------END CODE---------------------//   
+}
+    }
+}
