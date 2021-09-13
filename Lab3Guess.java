909f76407307314bd4a27a87741b9b542cb8fb4a
JnathanCarter <jcarter20@stu.jsu.edu>
2021-09-12 22:58:36 -0500

    fix shit

--- Lab3Guess.java
+++ Lab3Guess.java
@@ -17,42 +17,45 @@ public class Lab3Guess {
         
     Scanner Input = new Scanner(System.in);
         
-double rand = Math.random(); 
-        
-int num, tries=0, guess;
-int max = 100;
-int min = 1;
-int range = max - min + 1;
+    double rand = Math.random(); 
+            
+     int num; 
+     int tries=0;
+     int guess;
+     int max = 100;
+     int min = 1;
+     int range = max - min + 1;
   
 //GENERATES RANDOM NUMBER BETWEEN 1 AND 100
-for (int i = 0; i < 100; i++) {
-     num = (int)(Math.random() * range) + min;
-        
-        System.out.println("I'm thinking of a number between 1 and 100 ...\n");
-        System.out.println("Please guess a number; ");
-        guess = Input.nextInt();
-        
+    for (int i = 0; i < 100; i++) {
+        num = (int)(Math.random() * range) + min;
+            
+            System.out.println("I'm thinking of a number between 1 and 100 ...\n");
+            System.out.println("Please guess a number; ");
+            guess = Input.nextInt();
+        }        
 
     while (true) {
 
-	if (guess == num) 
-        {
-            System.out.println("Congratulations! You guessed the number in "+tries+" guess(es)!");
-            System.exit(1);
-    	} 
-	else if (guess > num)
-	{ 
-            System.out.println(" "+guess+" is too high.\n");
-            guess = Input.nextInt();
-	} 
-        else if (guess < num) 
+        if (guess == num) 
+            {
+                System.out.println("Congratulations! You guessed the number in "+tries+" guess(es)!");
+                System.exit(1);
+            } 
+        else if (guess > num)
         { 
-            System.out.println(" "+guess+" is too low.\n");
-            guess = Input.nextInt();
-	}	 
-	tries+=1; 
-        }
+                System.out.println(" "+guess+" is too high.\n");
+                guess = Input.nextInt();
+        } 
+            else if (guess < num) 
+            { 
+                System.out.println(" "+guess+" is too low.\n");
+                guess = Input.nextInt();
+        }	 
+        tries+=1; 
+        
+    }
  //-------------------END CODE---------------------//   
-}
+
     }
 }
